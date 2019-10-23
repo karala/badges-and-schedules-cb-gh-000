5b08c03ccbbe82db548ce3df29f0cc9fd3f04cd4
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-23 09:36:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,12 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)
-  name.each do |name|
-    array_of_names = Array.new(name)
+def batch_badge_creator(names)
+  sentences = []
+  names.each do |name|
+    sentences << "Hello, my name is #{name}."
   end
-  array_of_names
+  sentences
 end
 
 def assign_rooms(list)
