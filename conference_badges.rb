9321026e4113a74b219c683978dc088032ffa24c
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-23 09:13:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,14 @@
 # Write your code here.
-def batch_badge_creator(names)
-  names.each do |name|
-    puts "Hello, my name is #{name}."
-  end
-
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
 end
+
+# def batch_badge_creator(names)
+#   names.each do |name|
+#     puts "Hello, my name is #{name}."
+#   end
+# 
+# end
 
 def assign_rooms(list)
 
